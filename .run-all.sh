959d810afc5fff167ffdf51770aeaaf4060e1ce8
Gavin P. Salam <gavin.salam@physics.ox.ac.uk>
2025-02-23 16:27:18 +0000

    renamed pdf-rapdist.py -> lumi-rapdist.py

--- .run-all.sh
+++ .run-all.sh
@@ -2,7 +2,7 @@
 # short script to quickly check if all executables run OK.
 # NB: does not test anything beyond that
 
-for execs in pdf.py read_lhapdf.py mom.py lumi.py
+for execs in pdf.py read_lhapdf.py mom.py lumi.py lumi-rapdist.py
 do 
     ./$execs  > /dev/null && echo "$execs runs OK"
 done
